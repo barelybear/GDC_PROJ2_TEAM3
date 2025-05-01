472f645cd1fe9a72d15bcde59b068687114d3186
wuann15325 <159900518+wuann15325@users.noreply.github.com>
2025-05-01 14:04:09 +0700

    them mainmenu

---
+++ Assets/script/MainMenu/MenuManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Button startButton;
+    [SerializeField] Button guideButton;
+    [SerializeField] GameObject GuidePanel;
+    [SerializeField] Button continueButton;
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        guideButton.onClick.AddListener(OpenGuidePanel);
+        continueButton.onClick.AddListener(ContinueGame);
+        GuidePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenGuidePanel()
+    {
+        bool isActive = GuidePanel.activeSelf;
+        GuidePanel.SetActive(!isActive);
+    }
+
+    public void ContinueGame()
+    {
+        Debug.Log("Continue Game button clicked. Implement your logic here.");
+    }
+    
+}
